be9ad5b212a0a64ffb3031cadd70d6655e143d98
marottozara <marottozara@gmail.com>
2018-06-05 13:48:52 +0100

    Update in Studio scene: implemented screen interaction

--- Assets/MyScripts/TvCanvas.cs
+++ Assets/MyScripts/TvCanvas.cs
@@ -6,13 +6,34 @@ using UnityEngine.UI;
 
 public class TvCanvas : MonoBehaviour {
 
-	public GameObject thisCanvas01;
-    public GameObject panel;
-  
+	public GameObject PanelIntroduzione;
+    public GameObject PanelInizioDomande;
+    public GameObject PanelDomanda1;
+    public GameObject PanelRisposta1;
+    public GameObject PanelDomanda2;
+    public GameObject PanelRisposta2;
+    public GameObject PanelDomanda3;
+    public GameObject PanelRisposta3;
+    public GameObject PanelDomanda4;
+    public GameObject PanelRisposta4;
+
+    public ArrayList arrayPanels = new ArrayList();
+    
+
+
     // Use this for initialization
     void Start () {
-	
-	}
+        arrayPanels.Add(PanelIntroduzione);
+        arrayPanels.Add(PanelInizioDomande);
+        arrayPanels.Add(PanelDomanda1);
+        arrayPanels.Add(PanelRisposta1);
+        arrayPanels.Add(PanelDomanda2);
+        arrayPanels.Add(PanelRisposta2);
+        arrayPanels.Add(PanelDomanda3);
+        arrayPanels.Add(PanelRisposta3);
+        arrayPanels.Add(PanelDomanda4);
+        arrayPanels.Add(PanelRisposta4);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,9 +41,31 @@ public class TvCanvas : MonoBehaviour {
 	}
 
 
-	public void NextPage()
+	public void NextButton()
 	{
-		thisCanvas01.SetActive (false);
+        for (int i = 0; i < arrayPanels.Count; i++) {
+            if (((GameObject)arrayPanels[i]).activeSelf) {
+                ((GameObject)arrayPanels[i]).SetActive(false);
+                ((GameObject)arrayPanels[i+1]).SetActive(true);
+                break;
+            }
+        }
+        
+        /*PanelIntroduzione.SetActive (false);
+        PanelInizioDomande.SetActive(true);*/
+    }
+
+    public void BackButton()
+    {
+        for (int i = 0; i < arrayPanels.Count; i++)
+        {
+            if (((GameObject)arrayPanels[i]).activeSelf)
+            {
+                ((GameObject)arrayPanels[i]).SetActive(false);
+                ((GameObject)arrayPanels[i - 1]).SetActive(true);
+                break;
+            }
+        }
     }
 
 
