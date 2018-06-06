a9ce78a015dfb904a836cf67bad7b350d752da35
marottozara <marottozara@gmail.com>
2018-06-06 14:47:16 +0100

    Update in Studio scene: updated screen interaction and added music in scene

--- Assets/MyScripts/TvCanvas.cs
+++ Assets/MyScripts/TvCanvas.cs
@@ -16,13 +16,30 @@ public class TvCanvas : MonoBehaviour {
     public GameObject PanelRisposta3;
     public GameObject PanelDomanda4;
     public GameObject PanelRisposta4;
+    public GameObject PanelIntroFotoRagni;
+    public GameObject PanelFotoRagni01;
+    public GameObject PanelFotoRagni02;
+    public GameObject PanelFotoRagni03;
+    public GameObject PanelFotoRagni04;
+    public GameObject PanelFotoRagni05;
+    public GameObject PanelFotoRagni06;
+    public GameObject PanelFotoRagni07;
+    public GameObject PanelFotoRagni08;
+    public GameObject PanelFotoRagni09;
+    public GameObject PanelFotoRagni10;
+    public GameObject PanelFotoRagni11;
+
+    public GameObject PanelSospendiFotoRagni;
 
     public ArrayList arrayPanels = new ArrayList();
+
+    int indexArrayPanel;
     
 
 
     // Use this for initialization
     void Start () {
+        indexArrayPanel = 0;
         arrayPanels.Add(PanelIntroduzione);
         arrayPanels.Add(PanelInizioDomande);
         arrayPanels.Add(PanelDomanda1);
@@ -33,10 +50,22 @@ public class TvCanvas : MonoBehaviour {
         arrayPanels.Add(PanelRisposta3);
         arrayPanels.Add(PanelDomanda4);
         arrayPanels.Add(PanelRisposta4);
+        arrayPanels.Add(PanelIntroFotoRagni);
+        arrayPanels.Add(PanelFotoRagni01);
+        arrayPanels.Add(PanelFotoRagni02);
+        arrayPanels.Add(PanelFotoRagni03);
+        arrayPanels.Add(PanelFotoRagni04);
+        arrayPanels.Add(PanelFotoRagni05);
+        arrayPanels.Add(PanelFotoRagni06);
+        arrayPanels.Add(PanelFotoRagni07);
+        arrayPanels.Add(PanelFotoRagni08);
+        arrayPanels.Add(PanelFotoRagni09);
+        arrayPanels.Add(PanelFotoRagni10);
+        arrayPanels.Add(PanelFotoRagni11);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
@@ -51,8 +80,6 @@ public class TvCanvas : MonoBehaviour {
             }
         }
         
-        /*PanelIntroduzione.SetActive (false);
-        PanelInizioDomande.SetActive(true);*/
     }
 
     public void BackButton()
@@ -66,6 +93,34 @@ public class TvCanvas : MonoBehaviour {
                 break;
             }
         }
+    }
+
+
+    public void ExitPhotosButton()
+    {
+        for (int i = 0; i < arrayPanels.Count; i++)
+        {
+            if (((GameObject)arrayPanels[i]).activeSelf)
+            {
+                indexArrayPanel = i;
+                ((GameObject)arrayPanels[i]).SetActive(false);
+                PanelSospendiFotoRagni.SetActive(true);
+                break;
+            }
+        }
+    }
+
+
+    public void ResumePhotoButton()
+    {
+        PanelSospendiFotoRagni.SetActive(false);
+        ((GameObject)arrayPanels[indexArrayPanel]).SetActive(true);
+    }
+
+
+    public void ShutDownButton()
+    {
+        PanelSospendiFotoRagni.SetActive(false);
     }
 
 
