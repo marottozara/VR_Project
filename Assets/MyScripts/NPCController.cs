82c90f9af2a0bfb7c3ab520483430f1f27aef01d
marottozara <marottozara@gmail.com>
2018-06-18 13:42:00 +0100

    Update in Studio scene: fixed bugs in spider script and added theca box collider

---
+++ Assets/MyScripts/NPCController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NPCController : MonoBehaviour
+{
+    private float timeToChangeDirection;
+    public Rigidbody spider;
+
+    // Use this for initialization
+    public void Start()
+    {
+        ChangeDirection();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        timeToChangeDirection -= Time.deltaTime;
+
+        if (timeToChangeDirection <= 0)
+        {
+            ChangeDirection();
+        }
+
+        spider.velocity = transform.forward * 0.1f;
+    }
+
+
+
+    private void ChangeDirection()
+    {
+        float angle = Random.Range(0f, 360f);
+        Quaternion quat = Quaternion.AngleAxis(angle, Vector3.forward);
+        Vector3 newUp = quat * Vector3.up;
+        newUp.z = 0;
+        newUp.Normalize();
+        transform.up = newUp;
+        timeToChangeDirection = 1.5f;
+    }
+}
--- Assets/MyScripts/SpiderController.cs
+++ Assets/MyScripts/SpiderController.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class SpiderController : MonoBehaviour {
 
     static Animator anim;
-    public float speed = 2.0f;
-    public float rotationSpeed = 75.0f;
+    float speed = 0.01f;
+    float rotationSpeed = 75.0f;
+    public float rotationY;
 
 	// Use this for initialization
 	void Start () {
@@ -17,13 +18,14 @@ public class SpiderController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        float translation = /*Input.GetAxis("Vertical")*/transform.position.z * speed;
+        rotationY = Random.Range(-360f, 360f);
+       // float rotation = /*Input.GetAxis("Horizontal")*/transform.rotation.y * rotationSpeed;
         translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
+        rotationY *=  Time.deltaTime ;
 
         transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
+        transform.Rotate(0, rotationY, 0);
         if(translation != 0)
         {
             anim.SetBool("IsWalking", true);
