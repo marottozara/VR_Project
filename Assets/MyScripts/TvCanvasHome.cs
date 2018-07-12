2719b0f72f2a934a602a6f4be49f97b809785af5
marottozara <marottozara@gmail.com>
2018-07-12 09:57:10 +0100

    Update in Mountain scene: added panel for VR interaction

--- Assets/MyScripts/TvCanvasHome.cs
+++ Assets/MyScripts/TvCanvasHome.cs
@@ -3,21 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TvCanvasHome : MonoBehaviour {
+public class TvCanvasHome : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject PanelHome;
+
+    public GameObject PanelAcrofobia01;
+    public GameObject PanelAcrofobia02;
+
+    public ArrayList arrayAcrofobiaPanels = new ArrayList();
+
+    // Use this for initialization
+    void Start()
+    {
+
+        arrayAcrofobiaPanels.Add(PanelAcrofobia01);
+        arrayAcrofobiaPanels.Add(PanelAcrofobia02);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
 
     public void AracnofobiaButton()
     {
         SceneManager.LoadScene("Studio");
+    }
+
+
+    public void AcrofobiaButton()
+    {
+        PanelHome.SetActive(false);
+        PanelAcrofobia01.SetActive(true);
+    }
+
+
+    public void AcrofobiaNextButton()
+    {
+        for (int i = 0; i < arrayAcrofobiaPanels.Count; i++)
+        {
+            if (((GameObject)arrayAcrofobiaPanels[i]).activeSelf)
+            {
+                ((GameObject)arrayAcrofobiaPanels[i]).SetActive(false);
+                ((GameObject)arrayAcrofobiaPanels[i + 1]).SetActive(true);
+                break;
+            }
+        }
+    }
+
+
+    public void AcrofobiaBackButton()
+    {
+        for (int i = 0; i < arrayAcrofobiaPanels.Count; i++)
+        {
+            if (((GameObject)arrayAcrofobiaPanels[i]).activeSelf)
+            {
+                ((GameObject)arrayAcrofobiaPanels[i]).SetActive(false);
+                if (i == 0)
+                {
+                    PanelHome.SetActive(true);
+                }
+                else
+                {
+                    ((GameObject)arrayAcrofobiaPanels[i - 1]).SetActive(true);
+                }
+                break;
+            }
+        }
+    }
+
+
+    public void AcrofobiaStartButton()
+    {
+        SceneManager.LoadScene("Mountain");
     }
 }
